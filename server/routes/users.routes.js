b75f59e59b59c9d5db94063d0964f3b9e898e067
Joshua Desroches <josh.des.21@gmail.com>
2025-06-10 13:46:26 -0400

    10/06/25 - Corrected user routes issue

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -9,5 +9,9 @@ router.route('/api/users/:userId')
     .get(authCtrl.requireSignin, userCtrl.read)
     .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
     .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove)
+
+router.route('/api/users/:userId').get(userCtrl.read)
+router.route('/api/users/:userId').put(userCtrl.update)
+router.route('/api/users/:userId').delete(userCtrl.remove)
 router.param('userId', userCtrl.userByID)
 export default router
