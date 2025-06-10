bace6b9859c95f3cec76eb21591c6ec80f53a9dd
Joshua Desroches <josh.des.21@gmail.com>
2025-06-10 14:51:47 -0400

    10/06/25 - Assignment2 Complete

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -2,16 +2,12 @@ import express from 'express'
 import userCtrl from '../controllers/users.controller.js'
 import authCtrl from '../controllers/auth.controller.js'
 const router = express.Router()
-router.route('/api/users')
+router.route('/')
 .get(userCtrl.list)
 .post(userCtrl.create)
-router.route('/api/users/:userId')
+router.route('/:userId')
     .get(authCtrl.requireSignin, userCtrl.read)
     .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
     .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove)
-
-router.route('/api/users/:userId').get(userCtrl.read)
-router.route('/api/users/:userId').put(userCtrl.update)
-router.route('/api/users/:userId').delete(userCtrl.remove)
 router.param('userId', userCtrl.userByID)
 export default router
