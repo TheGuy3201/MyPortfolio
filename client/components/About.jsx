cb80b88d51d0f6891784fc92cda8e3e23bbd7c6b
Joshua Desroches <josh.des.21@gmail.com>
2025-10-10 23:38:57 -0400

    Updated About Page to relevant info

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -37,7 +37,7 @@ const About = memo(() => {
             {/* The right panel with a little introduction to me */}
             <div className="Description">
                 <p>My name is Joshua Desroches</p>
-                <p>I am a 2nd year software engineering technology student at Centennial College, specializing in Game Programming. Throughout my time at Centennial College, I have developed skills in a variety of programming languages such as C#, Java, JavaScript, HTML/CSS, C++, MySQL. With these skills, I have exceeded expectations, overcome challenges, and completed various tasks. In addition, I believe in bringing a friendly and positive attitude towards work and relationships to build strong connections and to succeed in anything I work towards.</p>
+                <p>I am a 3rd year software engineering technology student at Centennial College, specializing in Game Programming. Throughout my time at Centennial College, I have developed skills in a variety of programming languages such as C#, Java, JavaScript, HTML/CSS, Kotlin, C++, MySQL and many more. With these skills, I have exceeded expectations, overcome challenges, and completed various projects. In addition, I believe in bringing a friendly and positive attitude towards work and relationships to build strong connections and to succeed in anything I work towards.</p>
             </div>
         </div>
     </>
